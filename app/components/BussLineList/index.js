0a363e9ac3222c56aebfc60934c1011e9460abcf
agrup <agustinrodriguez206@gmail.com>
2020-03-18 22:32:27 -0300

    refactor buss lines list

--- app/components/BussLineList/index.js
+++ app/components/BussLineList/index.js
@@ -1,39 +1,52 @@
-import React from 'react';
-import { View, Text, Image, FlatList } from 'react-native';
+import * as React from 'react';
+import { useState, useEffect} from "react";
+import { SafeAreaView, Text, Image, FlatList } from 'react-native';
 import BussLineItem from './BussLineItem';
 import {mockBussLine} from '../../services/MockBussLineService';
 
-class BussLineList extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      data: this._addKeysToItems(mockBussLine)
-    };
-  }
-  _handleOnPress = id => {
-    
-  }
+import { firebaseApp } from "../../utils/FireBase";
+import * as firebase from "firebase";
+import "firebase/firestore";
+const db = firebase.firestore(firebaseApp);
 
-  _renderItem = ({item}) => {      
-    return(
-      <BussLineItem
-        name={item.name} 
-        color={item.color}
-        onPress={this._handleOnPress(item.id)}
-      />  
-    );
-  }
+export default function BussLineList () {
 
-  _addKeysToItems = items => {
-    return items.map(item => {
-      return Object.assign(item, {key: item.id} );
-    });
-  }
-  render(){
-    return (
-      <FlatList data={this.state.data} renderItem={this._renderItem} />
-    );
-  }
-}
+  const [login, setLogin] = useState(null);
+  const [Busslines, setBussLines] = useState({});
+  const [user, setUser] = useState(null);
   
-export default BussLineList;
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => {
+      !user ? setLogin(false) : setLogin(true);
+    });
+  }, []);
+
+  useEffect(() => {
+      const resultBussLines = [];
+      db.collection("BussLines").get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            console.log(`${doc.id} => ${doc.data()}`);
+        });
+   });
+
+   setBussLines(mockBussLine)
+
+   }, []);
+
+  return (
+    
+    <SafeAreaView>  
+      
+      <FlatList 
+        data={Busslines}
+        renderItem= { ({item}) => 
+          <BussLineItem
+            name={item.name} 
+            color={item.color}
+          />  
+      } />
+    </SafeAreaView>
+  );
+}
+;
---
+++ app/utils/FireBase.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase'
+
+//<!-- The core Firebase JS SDK is always required and must be listed first -->
+// <script src="https://www.gstatic.com/firebasejs/7.11.0/firebase-app.js"></script>
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#available-libraries -->
+// <script src="https://www.gstatic.com/firebasejs/7.11.0/firebase-analytics.js"></script>
+
+// <script>
+//   // Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCN80VlmOG_mEkxn4EMA5DR3XRGGL0vMV0",
+    authDomain: "arduinohome-1e847.firebaseapp.com",
+    databaseURL: "https://arduinohome-1e847.firebaseio.com",
+    projectId: "arduinohome-1e847",
+    storageBucket: "arduinohome-1e847.appspot.com",
+    messagingSenderId: "344724584812",
+    appId: "1:344724584812:web:db2ad928f3816599715b49",
+    measurementId: "G-0DF9X481YL"
+  };
+export const firebaseApp = firebase.initializeApp(firebaseConfig);
